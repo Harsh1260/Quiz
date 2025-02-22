f1ed7cad5c6ca4214a571600ed96a047e5478110
harsh1260 <harsh.jain513@nmims.edu.in>
2025-02-22 23:28:37 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: '0.0.0.0',  // Exposes to the local network
+    port: 5173,       // Ensure this port is open
+    strictPort: true, // Ensures Vite doesn’t switch to another port
+  }
 })
